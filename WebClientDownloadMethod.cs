69717d9183edc995dba70b8ae607132411f91691
eempc <evc484@gmail.com>
2019-05-23 18:04:04 +0100

    httpclient

--- WebClientDownloadMethod.cs
+++ WebClientDownloadMethod.cs
@@ -1,4 +1,6 @@
-        public string WebClientDownload(string url) {
-            WebClient w = new WebClient();
-            return w.DownloadString(url);
-        }
+public string WebClientDownload(string url) {
+    WebClient w = new WebClient();
+    return w.DownloadString(url);
+}
+
+// Has this been superceded by httpclient?

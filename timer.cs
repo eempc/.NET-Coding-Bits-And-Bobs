9a171da0d73bf30a60f194d51dbfd74255521751
Evelyn <evc484@gmail.com>
2019-05-31 11:30:02 +0100

    Create timer.cs

---
+++ timer.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Timers;
+
+namespace TimerTest {
+    class Program {
+        private static Timer timer;
+        static void Main(string[] args) {
+            Console.WriteLine("Started at {0:HH:mm:ss.fff}", DateTime.Now);
+            SetTimer();
+
+            Console.ReadKey();
+            timer.Stop();
+            timer.Dispose();
+        }
+
+        public static void SetTimer() {
+            timer = new Timer(2000);
+            timer.Elapsed += OnTimedEvent;
+            timer.AutoReset = true;
+            timer.Enabled = true;
+        }
+
+        private static void OnTimedEvent(Object sender, ElapsedEventArgs e) {
+            Console.WriteLine("Elapsed: {0:HH:mm:ss.fff}", e.SignalTime);
+        }
+    }
+}

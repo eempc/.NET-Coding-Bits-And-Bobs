171b23e7c18d4a6c07a775bfbafc1192b096aba5
Evelyn <evc484@gmail.com>
2019-06-09 14:59:49 +0100

    Update streamwriter.cs

--- streamwriter.cs
+++ streamwriter.cs
@@ -1,26 +1,26 @@
-        public static string musicFolder = @"D:\Music";
-        public static string m3uFileName = @"OllieKins.m3u";
-        public static string pathM3Ufile = Path.Combine(musicFolder, m3uFileName);
+public static string musicFolder = @"D:\Music";
+public static string m3uFileName = @"OllieKins.m3u";
+public static string pathM3Ufile = Path.Combine(musicFolder, m3uFileName);
 
-        static void Main(string[] args) {
-            if (!File.Exists(pathM3Ufile)) {
-                using (StreamWriter sw = File.CreateText(pathM3Ufile)) {
-                    sw.WriteLine("#EXTM3U");
-                }
-            }
-
-            if (args != null && args.Length > 0) {
-                    AddToM3U(args);
-            }
+static void Main(string[] args) {
+    if (!File.Exists(pathM3Ufile)) {
+        using (StreamWriter sw = File.CreateText(pathM3Ufile)) {
+            sw.WriteLine("#EXTM3U");
         }
+    }
 
-        public static void AddToM3U(string[] args) {
-            using (StreamWriter sw = File.AppendText(pathM3Ufile)) {
-                foreach (string path in args) {
-                    string fileName = Path.GetFileNameWithoutExtension(path);
-                    string relativePath = path.Substring(musicFolder.Length + 1);
-                    sw.WriteLine("#EXTINF:-1," + fileName + "\r\n" + relativePath);
-                    
-                }
-            }
+    if (args != null && args.Length > 0) {
+            AddToM3U(args);
+    }
+}
+
+public static void AddToM3U(string[] args) {
+    using (StreamWriter sw = File.AppendText(pathM3Ufile)) {
+        foreach (string path in args) {
+            string fileName = Path.GetFileNameWithoutExtension(path);
+            string relativePath = path.Substring(musicFolder.Length + 1);
+            sw.WriteLine("#EXTINF:-1," + fileName + "\r\n" + relativePath);
+
         }
+    }
+}

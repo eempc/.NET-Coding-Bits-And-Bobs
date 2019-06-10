78f2be44d44490fd8d6def294609fb666a68513a
Evelyn <evc484@gmail.com>
2019-06-10 13:34:03 +0100

    Update Program.cs

--- PDFCombine/PDFCombine/Program.cs
+++ PDFCombine/PDFCombine/Program.cs
@@ -4,9 +4,9 @@ using PdfSharp.Pdf.IO;
 namespace PDFCombine {
     class Program {
         static void Main(string[] args) {
-            string file1 = @"D:\CV\Verint\CoverLetter.pdf";
-            string file2 = @"D:\CV\Verint\cv.pdf";
-            string savePath = @"D:\CV\Verint\ChengE-CombinedLetterCV.pdf";
+            string file1 = @"D:\pdf1.pdf";
+            string file2 = @"D:\pdf2.pdf";
+            string savePath = @"D:\savefile.pdf";
 
             using (PdfDocument one = PdfReader.Open(file1, PdfDocumentOpenMode.Import))
             using (PdfDocument two = PdfReader.Open(file2, PdfDocumentOpenMode.Import))

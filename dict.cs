ddf4282a3b5bf3e5fd36807f415d82d0b006a533
Evelyn <evc484@gmail.com>
2019-06-19 14:58:00 +0100

    Update dict.cs

--- dict.cs
+++ dict.cs
@@ -1,5 +1,14 @@
             Dictionary<string, int> dict = new Dictionary<string, int>();
+
             foreach (KeyValuePair<string, int> item in dict) {
                 Console.WriteLine(item.Key);
                 Console.WriteLine(item.Value);
+            }
+
+            if (dict.TryGetValue("Key 0", out int x)) {
+
+            }
+
+            if (dict.ContainsKey("Key 0")) {
+
             }

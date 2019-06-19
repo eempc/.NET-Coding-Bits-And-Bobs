213bdfb2362e4a6c7d4443d030166473e805e7cf
Evelyn <evc484@gmail.com>
2019-06-19 19:46:04 +0100

    Update array.cs

--- array.cs
+++ array.cs
@@ -1,5 +1,5 @@
 int[][] jaggedArray = new int[2][3];
-int[,] multiDimensionalArray = new int[3,3];
+int[,] multiDimensionalArray = new int[3,4]; // must be rectangle
 
 return jaggedArray[i][j];
 return multiDimensionalArray[i, j];

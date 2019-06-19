d94fa76ce29f801860c0040be89239f0330ef1c0
Evelyn <evc484@gmail.com>
2019-06-19 12:14:43 +0100

    Update dict.cs

--- dict.cs
+++ dict.cs
@@ -1,5 +1,5 @@
             Dictionary<string, int> dict = new Dictionary<string, int>();
-            
             foreach (KeyValuePair<string, int> item in dict) {
-
+                Console.WriteLine(item.Key);
+                Console.WriteLine(item.Value);
             }

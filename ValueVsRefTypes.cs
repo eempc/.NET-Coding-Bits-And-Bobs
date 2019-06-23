c2a07ebba4278c3ccaf2017c387ed388bbf4b295
Evelyn <evc484@gmail.com>
2019-06-23 16:16:44 +0100

    Update ValueVsRefTypes.cs

--- ValueVsRefTypes.cs
+++ ValueVsRefTypes.cs
@@ -2,11 +2,11 @@
 int x = 5;
 int y = x;
 x = 10;
-Console.WriteLine(y.ToString());
-Console.WriteLine(x.ToString());
+Console.WriteLine(y.ToString()); // is 5
+Console.WriteLine(x.ToString()); // is 10
 
 // Reference type, use Object.ReferenceEquals(a, b) for assert tests
 Book book1 = new Book("Cat");
 Book book2 = book1;
 book2.Name = "Dog";
-Console.WriteLine(book1.Name);
+Console.WriteLine(book1.Name); // Is dog, not cat

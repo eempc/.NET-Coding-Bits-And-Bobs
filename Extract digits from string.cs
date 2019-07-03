35480f2a2db9ab633860fa1874e797019a2f9008
Evelyn <evc484@gmail.com>
2019-07-03 18:58:56 +0100

    Update Extract digits from string.cs

--- Extract digits from string.cs
+++ Extract digits from string.cs
@@ -1,26 +1,26 @@
-        //Three methods for doing so
-        
-        public string ExtractDigitsRecursive(string str) {
-            if (str.Length < 1)
-                return "";
-            else if (int.TryParse(str.Substring(0, 1), out int x))
-                return str.Substring(0, 1) + ExtractDigitsRecursive(str.Substring(1));
-            else
-                return ExtractDigitsRecursive(str.Substring(1));
-        }
+//Three methods for doing so
 
-        public string ExtractDigitsLoop1(string str) {
-            string result = String.Empty;
-            foreach (char c in str) {
-                if (c >= '0' && c <= '9') result += c;
-            }
-            return result;
-        }
+public string ExtractDigitsRecursive(string str) {
+    if (str.Length < 1)
+        return "";
+    else if (int.TryParse(str.Substring(0, 1), out int x))
+        return str.Substring(0, 1) + ExtractDigitsRecursive(str.Substring(1));
+    else
+        return ExtractDigitsRecursive(str.Substring(1));
+}
 
-        public string ExtractDigitsLoop2(string str) {
-            string result = String.Empty;
-            foreach (char c in str) {
-                if (Char.IsDigit(c)) result += c;
-            }
-            return result;
-        }
+public string ExtractDigitsLoop1(string str) {
+    string result = String.Empty;
+    foreach (char c in str) {
+        if (c >= '0' && c <= '9') result += c;
+    }
+    return result;
+}
+
+public string ExtractDigitsLoop2(string str) {
+    string result = String.Empty;
+    foreach (char c in str) {
+        if (Char.IsDigit(c)) result += c;
+    }
+    return result;
+}
